b72a72701650b799eb4d06acb605aa35a2d4f104
Orlando V Wiley Jr <owiley2002@gmail.com>
2020-12-12 19:42:22 -0500

    adding tool model

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,39 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes,Route, RouterModule } from '@angular/router';
 import { ContextContentComponent } from './context-content/context-content.component';
 import { ModelContentComponent } from './model-content/model-content.component';
 import { ServiceContentComponent } from './service-content/service-content.component';
 
-const routes: Routes = [
-  {path: 'model', component: ModelContentComponent},
-  {path: 'service', component: ServiceContentComponent},
-  {path: 'context', component: ContextContentComponent},
-  {path: '', redirectTo: '/context', pathMatch: 'full'}
+export interface Tool {
+  route: Route;
+  name:string;
+}
+
+const tools: Tool[] = [
+  {name: 'model', route: {path: 'model', component: ModelContentComponent}},
+  {name: 'service', route: {path: 'service', component: ServiceContentComponent}},
+  {name: 'context', route: {path: 'context', component: ContextContentComponent}}
 ];
+
+const routes: Routes = tools.map(tool=>tool.route).concat([
+  {path: '', redirectTo: '/model', pathMatch: 'full'}
+]);
+
+// [
+//   {path: 'model', component: ModelContentComponent},
+//   {path: 'service', component: ServiceContentComponent},
+//   {path: 'context', component: ContextContentComponent},
+//   {path: '', redirectTo: '/context', pathMatch: 'full'}
+// ];
+
+
+
+// const activeTool: Tool;
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
